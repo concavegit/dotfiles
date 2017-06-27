0c793c8c099cab1303f6d14b8c6c50f1a3b2cb4c
Kawin Nikomborirak <concavemail@gmail.com>
2017-06-27 00:56:51 +0000

    re-add offlineimap.py

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -50,6 +50,11 @@
 
 ;;; Orphans
 
+(defun offlineimap-get-password (host port)
+  (let* ((netrc (netrc-parse (expand-file-name "~/.netrc.gpg")))
+	 (hostentry (netrc-machine netrc host port port)))
+    (when hostentry (netrc-get hostentry "password"))))
+
 (setq backup-directory-alist '(("." . "~/.emacs.d/saves"))
       custom-file "~/.emacs.d/custom.el"
       inhibit-startup-screen t
