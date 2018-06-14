c2c7766e79619382dcb44784e25320e553158585
Kawin Nikomborirak <concavemail@gmail.com>
2018-06-14 04:57:54 +0000

    Evil-collection helm fixed

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -179,10 +179,7 @@
 
 ;;; Applications
 
-(use-package evil-collection :after evil
-  :config
-  (setq evil-collection-mode-list (remove 'helm evil-collection-mode-list))
-  (evil-collection-init))
+(use-package evil-collection :after evil :config (evil-collection-init))
 (use-package evil-mu4e :after mu4e)
 (use-package htmlize)
 (use-package nov :mode ("\\.epub$" . nov-mode))
